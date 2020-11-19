1795e68c12e5d159742e645efd3456fda416a0b0
sorididim11 <sorididim11@gmail.com>
2020-11-19 15:39:37 +0900

    update appspec.yml

---
+++ src/main/java/MessageUtil.java
@@ -0,0 +1,16 @@
+public class MessageUtil {
+  private String message;
+ public MessageUtil(String message) {
+    this.message = message;
+    }
+public String printMessage() {
+    System.out.println(message);
+    return message;
+  }
+ public String salutationMessage() {
+    message = "Hi!" + message;
+    System.out.println(message);
+    return message;
+  }
+}
+
